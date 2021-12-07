d7eb69221e8449200c2fba9da222c565d05e51ef
nicolasna77 <nicolas_abreu@live.fr>
2021-12-07 11:28:23 +0100

    Update appBar.js

--- src/componant/appBar.js
+++ src/componant/appBar.js
@@ -10,20 +10,20 @@ import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import MenuIcon from '@mui/icons-material/Menu';
 import Paper from '@mui/material/Paper';
-import  {Grid}  from '@mui/material';
-import  {useNavigate}  from 'react-router-dom';
+import { Grid } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import PersonAdd from '@mui/icons-material/PersonAdd';
 import LockIcon from '@mui/icons-material/Lock';
 import Typography from '@mui/material/Typography';
-import Chip from '@mui/material/Chip';
-
+import Button from '@mui/material/Button';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 export default function PrimarySearchAppBar() {
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -39,10 +39,10 @@ const navigate = useNavigate();
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar color="default" position="relative" style={{ flexWrap: 'wrap'}} >
+      <AppBar color="default" position="relative"  >
         <Toolbar>
-        <Box sx={{ display: { xs: 'flex', sm: 'none' } }}>
-          <IconButton
+          <Box sx={{ display: { xs: 'flex', sm: 'none' } }}>
+            <IconButton
               size="large"
               edge="start"
               color="inherit"
@@ -51,134 +51,136 @@ const navigate = useNavigate();
 
               <MenuIcon />
 
-          </IconButton>
+            </IconButton>
 
-        </Box>
+          </Box>
 
-        <div>
-        <img onClick={() => navigate('/')} class="logoHeader" alt="logo" src={process.env.PUBLIC_URL + '/logo.png'} /> 
-        </div>
+          <div>
+            <img onClick={() => navigate('/')} class="logoHeader" alt="logo" src={process.env.PUBLIC_URL + '/logo.png'} />
+          </div>
 
-   
-<Grid container justifyContent="center" sx={{ display: { xs: 'none', sm: 'flex' } }}>
+          <Box sx={{ marginLeft: "20px", alignSelf: "center" }} >
+            <Button size="large" variant="" color="primary" aria-label="menu" >
 
-            <Box sx={{marginRight:"20px", alignSelf:"center"}} >
-              <Chip color="primary" icon={<MenuIcon/> }  label="Categorie"   aria-label="menu" />
-            </Box>
-             
-               <Paper component="form" >
-            <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+              <Box sx={{ marginRight: "20px" }} >
+                <Typography variant="caption"> Categorie</Typography>
+              </Box>
+              <ArrowDropDownIcon />
+            </ Button>
+          </Box>
+
+          <Grid container justifyContent="center" sx={{ display: { xs: 'none', sm: 'flex' } }}>
+
+
+            <Paper component="form" elevation="0" sx={{ borderRadius: "26px", maxWidth: "600px", border: "1px solid #dbdbdb" }} >
+              <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
                 <SearchIcon />
               </IconButton>
-              <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Recherche" inputProps={{ 'aria-label': 'Recherche' }}
-              />
-             
+              <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Recherche" inputProps={{ 'aria-label': 'Recherche' }} />
+
 
             </Paper>
 
-  </Grid>
-    
-     
+          </Grid>
+
+
           <Box sx={{ flexGrow: 1 }} />
 
-          <Box sx={{ display: { xs: 'none', md: 'flex', sm:'flex' } }}>
+          <Box sx={{ display: { xs: 'none', md: 'flex', sm: 'flex' } }}>
             <IconButton onClick={() => navigate('/Panier')} size="large" >
               <Badge badgeContent={4} color="secondary">
                 <ShoppingCartIcon />
               </Badge>
             </IconButton>
-           
-            <IconButton size="large" onClick={handleClick}  > 
-               <PersonIcon/>
+
+            <IconButton size="large" onClick={handleClick}  >
+              <PersonIcon />
             </IconButton>
 
           </Box>
 
 
-        {/* version mobile */}
+          {/* version mobile */}
 
 
 
           <Box sx={{ display: { xs: 'flex', sm: 'none' } }}>
-          <IconButton size="large" >
-                <SearchIcon/>
+            <IconButton size="large" >
+              <SearchIcon />
             </IconButton>
-            
-          <IconButton size="large" onClick={() => navigate('/Panier')} > 
-          <Badge badgeContent={4} color="secondary" >
+
+            <IconButton size="large" onClick={() => navigate('/Panier')} >
+              <Badge badgeContent={4} color="secondary" >
                 <ShoppingCartIcon />
               </Badge>
             </IconButton>
 
 
-            <IconButton onClick={handleClick} size="large"  > 
-         <PersonIcon/>
+            <IconButton onClick={handleClick} size="large"  >
+              <PersonIcon />
             </IconButton>
           </Box>
 
 
           <Box sx={{ display: { xs: 'flex', sm: 'none' } }}>
-          <Menu
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        onClick={handleClose}
-        PaperProps={{
-          elevation: 1,
-          sx: {
-           
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-            mt: 1.5,
-            '& .MuiAvatar-root': {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            '&:before': {
-              display: 'block',
-              position: 'absolute',
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
+          
+            <Menu
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 1,
+                sx: {
 
-            },
-          },
-        }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-      >
-        
-        <MenuItem onClick={() => navigate('/Inscription')}>
-          <ListItemIcon>
-            <PersonAdd fontSize="small" />
-          </ListItemIcon>
-        S'inscrire
-        </MenuItem>
-        <MenuItem onClick={() => navigate('/Connection')}>
-          <ListItemIcon >
-            <LockIcon fontSize="small" />
-          </ListItemIcon>
-         Connection
-        </MenuItem>
-        
-      </Menu>
+                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                  mt: 1.5,
+                  '& .MuiAvatar-root': {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  '&:before': {
+                    display: 'block',
+                    position: 'absolute',
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: 'background.paper',
+                    transform: 'translateY(-50%) rotate(45deg)',
+
+                  },
+                },
+              }}
+
+              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }} >
+
+
+              <MenuItem onClick={() => navigate('/Inscription')}>
+                <ListItemIcon>
+                  <PersonAdd fontSize="small" />
+                </ListItemIcon>
+                S'inscrire
+              </MenuItem>
+              <MenuItem onClick={() => navigate('/Connection')}>
+                <ListItemIcon >
+                  <LockIcon fontSize="small" />
+                </ListItemIcon>
+                Connection
+              </MenuItem>
+
+            </Menu>
 
           </Box>
 
 
-
-
-
         </Toolbar>
 
-
-      
       </AppBar>
-    
+
     </Box>
   );
 }
