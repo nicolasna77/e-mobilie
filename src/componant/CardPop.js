7280d20586a3688d46b9d2ba4280496dfcf23187
nicolasna77 <nicolas_abreu@live.fr>
2021-12-12 12:37:25 +0100

    update

--- src/componant/CardPop.js
+++ src/componant/CardPop.js
@@ -1,170 +1,131 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import CardMedia from '@mui/material/CardMedia';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import Grid from '@mui/material/Grid';
-import Rating from '@mui/material/Rating';
-import CardActions from '@mui/material/CardActions';
-import Favorite from '@mui/icons-material/Favorite';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import RadioGroup from '@mui/material/RadioGroup';
-import PropTypes from 'prop-types';
-import  { useRadioGroup } from '@mui/material/RadioGroup';
-import Radio from '@mui/material/Radio';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Grow from '@mui/material/Grow';
+import React from "react";
+import Box from "@mui/material/Box";
+import CardMedia from "@mui/material/CardMedia";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import Grid from "@mui/material/Grid";
+import Rating from "@mui/material/Rating";
+import CardActions from "@mui/material/CardActions";
+import Favorite from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Grow from "@mui/material/Grow";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
-
-// api pour gerer le system de couleur pour un article 
-import { CirclePicker  } from 'react-color';
-
-
+// api pour gerer le system de couleur pour un article
+import { CirclePicker } from "react-color";
 
 class FavoriteButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            favoriteIcon: false,
+  constructor(props) {
+    super(props);
+    this.state = {
+      favoriteIcon: false,
+    };
+  }
+
+  render() {
+    return (
+      <IconButton
+        className="buttonFavorite"
+        onClick={() =>
+          this.setState({ favoriteIcon: !this.state.favoriteIcon })
         }
-    }
-
-    render() {
-        return (
-            <IconButton  className="buttonFavorite" onClick={() => this.setState({ favoriteIcon: !this.state.favoriteIcon })}>
-                {this.state.favoriteIcon ? <Favorite color='error' /> : <FavoriteBorderIcon color="action"/>
-                }
-
-            </IconButton>
-        );
-    }
-
+      >
+        {this.state.favoriteIcon ? (
+          <Favorite color="error" />
+        ) : (
+          <FavoriteBorderIcon color="action" />
+        )}
+      </IconButton>
+    );
+  }
 }
 
-function MyFormControlLabel(Props) {
+interface Props {
+  image: string;
+  title: string;
+  description: string;
+  stars: Number;
+  price: Number;
+  color: string;
+}
 
-  
+export default function CardPop({
+  title,
+  image,
+  description,
+  stars,
+  price,
+  color,
+}): props {
+  return (
+    <Grid item xs="6" sm="4" md="3">
+      <Grow in="to">
+        <Card elevation="0" className="cardProduct">
+          <FavoriteButton />
 
-    const radioGroup = useRadioGroup();
-    let checked = false;
-    if (radioGroup) {
-      checked = radioGroup.value === props.value;
-    }
-  
-    return <FormControlLabel checked={checked} {...props} />;
-  }
-  
-  MyFormControlLabel.propTypes = {
-    value: PropTypes.any,
-  };
+          <CardActionArea>
+            <CardMedia component="img" height="170" src={image} alt={title} />
 
+            <CardContent>
+              <Box>
+                <Rating
+                  value={stars}
+                  name="rating"
+                  size="small"
+                  readOnly
+                  precision={0.5}
+                />
+              </Box>
 
+              <Typography gutterBottom variant="subtitle2" component="div">
+                {title}
+              </Typography>
 
-  interface Props {
-    image: string;
-    title: string;
-    description: string;
-    stars: Number;
-    price: Number;
-    option: option[];
+              <Typography variant="body2" color="text.primary">
+                {description}
+              </Typography>
 
-  }
-
-
-export default function CardPop({title,image,description, stars, price, option}):props {
-  
-
-  return(
-
-
-                        <Grid item xs="6" sm="4" md="3" >
-                             <Grow in="to" >
-
-                            <Card elevation="0"  className="cardProduct">
-
-                                <FavoriteButton />
-
-
-                                <CardActionArea>
-                                    <CardMedia
-                                        component="img"
-                                        height="170"
-                                        src={image}
-                                        alt={`Preview of ${title}`}/>
-
-                                    <CardContent>
-                                      
-
-                                          
-                                                    <Box >
-                                                        <Rating
-                                                            value={stars}
-                                                            name="rating"
-                                                            size="small"
-                                                            readOnly
-                                                            precision={0.5}
-                                                        />
-                                                    </Box>
-                                            
-                                      
-
-                                           
-                                      
-                                        <Typography gutterBottom variant="subtitle2" component="div">
-                                            {title}
-                                        </Typography>
-
-                                        <Typography variant="body2" color="text.primary">
-                                           {description}
-                                        </Typography>
-
-                                        <Grid container alignItems="center" xs={12} md={12} spacing="3">
-
-                                            <Grid container xs={12} sm={12} md={12}  >
-
-                                              
-
-                                                   
-                                            <CardActions>
-                                                    <FormControl  >
-
-                                             
-                                                       
-                                                    {/* {option.keys(option).map((key) => {
+              <Grid container alignItems="center" xs={12} md={12} spacing="3">
+                <Grid container xs={12} sm={12} md={12}>
+                  <CardActions>
+                    <FormControl>
+                      {/* {option.keys(option).map((key) => {
 
                                                         return( */}
-                                                       <CirclePicker   circleSize={20} height="12px" colors={['#D9E3F0', '#F47373', '#697689']}/>
-                                                        {/* )})};  */}
-                                                       
-                                                        
-                                                
-                                                    </FormControl>
+                      <CirclePicker
+                        circleSize={20}
+                        height="12px"
+                        colors={["#D9E3F0", "#F47373", "#697689"]}
+                      />
+                      {/* )})};  */}
+                    </FormControl>
+                  </CardActions>
+                </Grid>
 
-</CardActions>
-                                           
-
-                                            
-                                            </Grid>
-
-
-                                            <Grid item  xs={12} sm={12} md={12} >
-                                                <Typography sx={{ float:"right"}} className="priceCardProduct" gutterBottom variant="h6" component="div">
-                                                    {price} €
-                                                </Typography>
-                                            </Grid>
-                                        </Grid>
-                                    </CardContent>
-                                </CardActionArea>
-
-                            </Card>
-                            </Grow>
-                        </Grid>
-
-              
+                <Grid item xs={12} sm={12} md={12}>
+                  <IconButton color="primary" aria-label="add to shopping cart">
+                    <AddShoppingCartIcon />
+                  </IconButton>{" "}
+                  <Typography
+                    sx={{ float: "right" }}
+                    className="priceCardProduct"
+                    gutterBottom
+                    variant="h6"
+                    component="div"
+                  >
+                    {price} €
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Grow>
+    </Grid>
   );
 }
-
--- src/componant/ListPanier.js
+++ src/componant/ListPanier.js
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import { Button, Divider, Input } from '@mui/material';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { Button, Divider, Input } from "@mui/material";
 // import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+import Typography from "@mui/material/Typography";
 // import DeleteIcon from '@mui/icons-material/Delete';
 // import MenuItem from '@mui/material/MenuItem';
 // import Select from '@mui/material/Select';
 // import InputLabel from '@mui/material/InputLabel';
 // import FormControl from '@mui/material/FormControl';
-import Paper from '@mui/material/Paper';
-import ListItemText from '@mui/material/ListItemText';
-import IconButton from '@mui/material/IconButton';
-import Grid from '@mui/material/Grid';
-import DeleteIcon from '@mui/icons-material/Delete';
-import ListItem from '@mui/material/ListItem';
+import Paper from "@mui/material/Paper";
+import ListItemText from "@mui/material/ListItemText";
+import IconButton from "@mui/material/IconButton";
+import Grid from "@mui/material/Grid";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ListItem from "@mui/material/ListItem";
 
 // const Img = styled('img')({
 //     margin: 'auto',
@@ -22,136 +22,124 @@ import ListItem from '@mui/material/ListItem';
 //     maxHeight: '100%',
 // });
 
-
-
-
 export default function ListPanier() {
-  
-    const [dense, setDense] = React.useState(false);
-    const [secondary, setSecondary] = React.useState(false);
-    return (
-        <Box pt={{ xs: 8, md: 8 }} sx={{margin: 'auto', maxWidth:"1300px !important"}}>
-            
-            <Grid container md={10}  sx={{ margin: 'auto', flexGrow: 1 }}>
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+  return (
+    <Box
+      pt={{ xs: 8, md: 8 }}
+      pb={{ xs: 8, md: 8 }}
+      sx={{ margin: "auto", maxWidth: "1300px !important" }}
+    >
+      <Grid container md={10} sx={{ margin: "auto", flexGrow: 1 }}>
+        <Grid
+          container
+          xs={12}
+          sm={10}
+          md={8}
+          sx={{ margin: "auto", flexGrow: 1 }}
+        >
+          <Grid item xs={10} sx={{ maxWidth: "700px !important" }}>
+            <Box>
+              <Typography variant="subtitle1" component="div">
+                Total Articles : 2 Articles
+              </Typography>
+            </Box>
+          </Grid>
 
-              <Grid container xs={12} sm={10} md={8} sx={{ margin: 'auto', flexGrow: 1}}>
+          <Grid container xs={12} md={12} sx={{ maxWidth: "700px !important" }}>
+            <ListItem>
+              <Grid item>
+                <ListItem>
+                  <img src="./salon.jpg" alt="" srcset="" width="120px" />
+                </ListItem>
+              </Grid>
 
-                <Grid item  xs={10} sx={{ maxWidth:"700px !important"}}>
-                    <Box >
-                        <Typography variant="subtitle1" component="div">Total Articles : 2 Articles</Typography>
-                      
-                    </Box>
-                </Grid>
+              <Grid item xs={7}>
+                <ListItemText primary="Table Salon" secondary="color : grey" />
+              </Grid>
 
+              <Grid item xs="3">
+                <Input
+                  defaultValue="1"
+                  sx={{ width: "35px" }}
+                  size="small"
+                  type="number"
+                ></Input>
+              </Grid>
 
-                    <Grid container xs={12} md={12}  sx={{ maxWidth:"700px !important"  }} >
-                        <ListItem   >
-                            <Grid item>
-                                <ListItem>
-                                    <img src="./salon.jpg" alt="" srcset="" width="120px" />
-                                </ListItem>
-                            </Grid>
+              <Grid item xs="3">
+                <Typography variant="h6">9.99€</Typography>
+              </Grid>
 
-                            <Grid item xs={7}>
-                                <ListItemText
-                                    primary="Table Salon"
-                                    secondary='color : grey'/>
-                            </Grid>
+              <Grid item>
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon color="error" />
+                </IconButton>
+              </Grid>
+            </ListItem>
 
-                            <Grid item xs="3">
-                                <Input defaultValue="1" sx={{ width:"35px"}} size="small" type="number"></Input>
-                            </Grid>
+            <ListItem>
+              <Grid item>
+                <ListItem>
+                  <img src="./SalleDeBain.jpg" alt="" srcset="" width="120px" />
+                </ListItem>
+              </Grid>
 
+              <Grid item xs={7}>
+                <ListItemText primary="Table Salon" secondary="color : grey" />
+              </Grid>
 
-                            <Grid item xs="3">
-                                <Typography variant="h6" >9.99€</Typography>
-                            </Grid>
+              <Grid item xs="3">
+                <Input
+                  defaultValue="1"
+                  sx={{ width: "35px" }}
+                  size="small"
+                  type="number"
+                ></Input>
+              </Grid>
 
-                            <Grid item>
-                                <IconButton edge="end" aria-label="delete">
-                                    <DeleteIcon color="error" />
-                                </IconButton>
-                            </Grid>
-  
-                        </ListItem>
+              <Grid item xs="3">
+                <Typography variant="h6">9.99€</Typography>
+              </Grid>
 
-                        <ListItem   >
-                            <Grid item>
-                                <ListItem>
-                                    <img src="./SalleDeBain.jpg" alt="" srcset="" width="120px" />
-                                </ListItem>
-                            </Grid>
+              <Grid item>
+                <IconButton edge="end" aria-label="delete">
+                  <DeleteIcon color="error" />
+                </IconButton>
+              </Grid>
+            </ListItem>
+          </Grid>
+        </Grid>
 
-                            <Grid item xs={7}>
-                                <ListItemText
-                                    primary="Table Salon"
-                                    secondary='color : grey'/>
-                            </Grid>
+        <Grid item xs={12} sm={12} md={3}>
+          <Box sx={{ width: "100%", textAlign: "center" }}>
+            <Paper>
+              <Box sx={{ width: "100%" }}>
+                <Box sx={{ width: "100%" }} mb="15px" mt="15px">
+                  <Typography variant="h6" component="span">
+                    Total :
+                  </Typography>
+                  <Typography variant="h6" component="span">
+                    {" "}
+                    9,99€
+                  </Typography>
+                </Box>
 
-                            <Grid item xs="3">
-                                <Input defaultValue="1" sx={{ width:"35px"}} size="small" type="number"></Input>
-                            </Grid>
-
-
-                            <Grid item xs="3">
-                                <Typography variant="h6" >9.99€</Typography>
-                            </Grid>
-
-                            <Grid item>
-                                <IconButton edge="end" aria-label="delete">
-                                    <DeleteIcon color="error" />
-                                </IconButton>
-                            </Grid>
-  
-                        </ListItem>
-
-                     
-
-                    </Grid>
-
-
-
- </Grid>
-
-                <Grid item xs={12} sm={12} md={3} >
-                    <Box sx={{ width: '100%', textAlign: 'center' }}>
-
-                        <Paper >
-                          
-                            <Box sx={{ width: '100%' }}    >
-
-                         
-
-                                <Box sx={{ width: '100%' }} mb="15px" mt="15px" >
-                                    <Typography variant="h6" component="span"  >Total :</Typography>
-                                    <Typography variant="h6" component="span"  > 9,99€</Typography>
-                                </Box>
-
-
-
-                                <Box sx={{ width: '100%' }}  >
-                                    <Button variant="contained" color="secondary"> Valider mon panier</Button>
-                                </Box>
-                                <Box sx={{ width: '100%' }} pt="10px" >
-                                    <Button color="secondary" > Continue mes achat</Button>
-                                </Box>
-                           
-                            </Box>
-
-                        </Paper>
-
-                    </Box>
-
-                </Grid>
-            </Grid>
-
-        </Box>
-
-
- )
-
-
-
-
-
+                <Box sx={{ width: "100%" }}>
+                  <Button variant="contained" color="secondary">
+                    {" "}
+                    Valider mon panier
+                  </Button>
+                </Box>
+                <Box sx={{ width: "100%" }} pt="10px">
+                  <Button color="secondary"> Continue mes achat</Button>
+                </Box>
+              </Box>
+            </Paper>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
